67488b9b85c41f456037b74a3d643c888db884ba
Hrishikesh <hdhamode5@gmail.com>
2024-04-22 01:08:51 +0530

    22/04/24

--- src/test/java/CucumberOptions/TestNGRunner.java
+++ src/test/java/CucumberOptions/TestNGRunner.java
@@ -6,7 +6,10 @@ import org.testng.annotations.DataProvider;
 
 //import cucumberOptions.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;  
+
+
+
 
 @CucumberOptions(
 		features="src/test/java/features",
@@ -16,6 +19,7 @@ import io.cucumber.testng.CucumberOptions;
 		plugin = {"html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 		 
 )
+
 public class TestNGRunner extends AbstractTestNGCucumberTests{
 	@Override
 	@DataProvider(parallel=true)
